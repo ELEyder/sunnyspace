e8c7aa35fd0c1a802e05c4b509bd0da059413492
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-04 02:58:34 -0500

    Errores de estilos arreglados

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,12 +5,23 @@ interface ButtonProps {
   type?: "button" | "submit" | "media";
   onClick?: () => void;
   block?: boolean;
+  style?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ children = 'Botón', type = undefined, onClick = undefined, block = false }) => {
+const Button: React.FC<ButtonProps> = (
+  { 
+    children = 'Botón',
+    type = undefined,
+    onClick = undefined,
+    block = false,
+    style = 'primary'
+  }) => {
+
   if (type === 'media') {
     return (
-      <label className={styles.button} htmlFor="media">
+      <label
+      className={style == 'primary' ? styles.button : styles.secondary}
+      htmlFor="media">
         {children}
         <input type="file" name="media" id="media" className={styles.media} accept=".jpg, .gif, .mp4" />
       </label>
@@ -19,7 +30,7 @@ const Button: React.FC<ButtonProps> = ({ children = 'Botón', type = undefined, 
   return (
     <button
       type={type}
-      className={styles.button}
+      className={style == 'primary' ? styles.button : styles.secondary}
       onClick={onClick}
       {...(block ? { style: { display: 'block', width: '100%' } } : {})}
     >
