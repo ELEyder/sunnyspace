2cba96f778c390842311fa12c9d36518f33dce10
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-04 02:43:22 -0500

    Nuevas opciones de privacidad a las plubicaciones

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,12 +2,20 @@ import styles from './Button.module.css';
 
 interface ButtonProps {
   children?: React.ReactNode;
-  type?: "button" | "submit" | "reset";
+  type?: "button" | "submit" | "media";
   onClick?: () => void;
   block?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({ children = 'Botón', type = undefined, onClick = undefined, block = false }) => {
+  if (type === 'media') {
+    return (
+      <label className={styles.button} htmlFor="media">
+        {children}
+        <input type="file" name="media" id="media" className={styles.media} accept=".jpg, .gif, .mp4" />
+      </label>
+    )
+  }
   return (
     <button
       type={type}
